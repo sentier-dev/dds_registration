c29b1ae6f949a18fc4c5d18951b39c61e248981a
lilliputten <lilliputten@yandex.ru>
2024-03-08 03:34:49 +0700

    Issue #2: Added dds asets (logo, background), updated styles for header navbar and body, added components-demo and robots.txt pages.

--- dds_registration/urls.py
+++ dds_registration/urls.py
@@ -1,10 +1,24 @@
 from django.urls import path
 from django.contrib import admin
+from django.views.decorators.cache import cache_page
+from django.conf import settings
+
 from . import views
+
+cache_timeout = 15 * 60  # in seconds: {min}*60
+if settings.LOCAL or settings.DEBUG:
+    cache_timeout = 0
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("", views.index, name="index"),
     path("me", views.events_mine, name="events_mine"),
     path("event/<str:code>", views.events_main, name="event_view"),
+
+    # Demo pages...
+    path('components-demo', views.components_demo, name='components_demo'),
+
+    # Service pages...
+    path('robots.txt', cache_page(cache_timeout)(views.RobotsView.as_view()), name='robots'),
+    #  url(r'^sitemap\.xml$', 'django.contrib.sitemaps.views.sitemap', {'sitemaps': sitemap.sitemaps_dict}, name='sitemap'),
 ]
--- dds_registration/views.py
+++ dds_registration/views.py
@@ -1,11 +1,19 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from django.http.response import Http404
+from django.http import HttpRequest, HttpResponse
+from django.views.generic import TemplateView
+from django.contrib.sites.models import Site
+import logging
+
 
 from .models import Event
 
 
-def index(request):
+LOG = logging.getLogger(__name__)
+
+
+def index(request: HttpRequest):
     if request.user.is_authenticated:
         return redirect("events_mine")
     else:
@@ -13,17 +21,54 @@ def index(request):
 
 
 @login_required
-def events_mine(request):
+def events_mine(request: HttpRequest):
     if not request.user.is_authenticated:
         return redirect("index")
 
     return render(request, "mine.html.django")
 
 
-def events_main(request, code):
+def events_main(request: HttpRequest, code):
     try:
         event = Event.objects.get(code=code)
     except Event.DoesNotExist:
         return Http404
 
     return render(request, "mine.html.django", {"event": event})
+
+
+def components_demo(request: HttpRequest):
+    return render(request, "components-demo.html.django")
+
+
+# Misc...
+
+
+class RobotsView(TemplateView):
+    template_name = 'robots.txt'
+    content_type = 'text/plain'
+    def get_context_data(self, **kwargs):
+        context = super(RobotsView, self).get_context_data(**kwargs)
+        context['domain'] = Site.objects.get_current().domain
+        return context
+
+
+# Error pages...
+
+
+def page403(request, *args, **argv):
+    LOG.debug('403 error')
+    return render(request, '403.html', {}, status=403)
+
+
+def page404(request, *args, **argv):
+    LOG.debug('404 error')
+    return render(request, '404.html', {}, status=404)
+
+
+def page500(request, *args, **argv):
+    LOG.debug('500 error')
+    return render(request, '500.html', {}, status=500)
+
+
+
