24047322a72d4128a259ac40ed0ab71b0031876b
lilliputten <lilliputten@gmail.com>
2024-03-20 13:10:51 +0700

    Issue #31: Updated version: 0.0.10.

--- dds_registration/__init__.py
+++ dds_registration/__init__.py
@@ -6,6 +6,6 @@ __all__ = (
 
 #  from .models import AppUser
 
-__version__ = "0.0.9"
-__timestamp__ = "2024.03.19 01:46 +0700"
-__timetag__ = "240319-0146"
+__version__ = "0.0.10"
+__timestamp__ = "2024.03.20 13:10 +0700"
+__timetag__ = "240320-1310"
