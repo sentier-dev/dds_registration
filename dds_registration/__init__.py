21651b70c5bd2a30c562655c63e24eacbb68f135
lilliputten <lilliputten@gmail.com>
2024-03-20 19:40:33 +0700

    Issue #42: Fixed previous errors.

--- dds_registration/__init__.py
+++ dds_registration/__init__.py
@@ -1,11 +1,10 @@
+__version__ = '0.0.10'
+__timestamp__ = '2024.03.20 13:10 +0700'
+__timetag__ = '240320-1310'
+
+
 __all__ = (
     __version__,
     __timestamp__,
     __timetag__,
 )
-
-#  from .models import AppUser
-
-__version__ = "0.0.11"
-__timestamp__ = "2024.03.20 18:19 +0700"
-__timetag__ = "240320-1819"
