4cdeef91ff67ccbf9a82e248c4817aa4984a2405
lilliputten <lilliputten@gmail.com>
2024-03-30 20:06:48 +0700

    Issue #73: Changed `dateFormat` to "%B %d, %Y": it'll produce date days with leading zeros ("March 01, 2024").
    TODO: To find a cross-platform solution for this problem.

--- dds_registration/core/constants/date_time_formats.py
+++ dds_registration/core/constants/date_time_formats.py
@@ -2,7 +2,7 @@
 
 # @see https://www.geeksforgeeks.org/python-strftime-function/
 dateTimeFormat = "%Y.%m.%d %H:%M %Z"
-dateFormat = "%B %#d, %Y"  # TODO: A day without zero? NOTE: It seems to be non-cross-platform: '%#d' is for windows and '%-d' -- for unix
+dateFormat = "%B %d, %Y"  # TODO: A day without zero? NOTE: It seems to be non-cross-platform: '%#d' is for windows and '%-d' -- for unix
 #  dateFormat = '%Y.%m.%d'
 dateTagFormat = "%y%m%d-%H%M"  # eg: '220208-0204'
 dateTagPreciseFormat = "%y%m%d-%H%M%S"  # eg: '220208-020423'
