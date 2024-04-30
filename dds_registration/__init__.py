12bd3c8c24bd57dbfac1ac156bb51c4b5b621169
Chris Mutel <cmutel@gmail.com>
2024-04-30 15:02:24 +0200

    1.0.10

--- dds_registration/__init__.py
+++ dds_registration/__init__.py
@@ -6,9 +6,9 @@ stripe.api_key = settings.STRIPE_SECRET_KEY  # 'sk_...'
 
 
 # Project version properties
-__version__ = "1.0.9"
-__timestamp__ = "2024.04.08 13:55 +0700"
-__timetag__ = "240408-1355"
+__version__ = "1.0.10"
+__timestamp__ = "2024.04.30 15:01 +0200"
+__timetag__ = "240430-1501"
 
 
 __all__ = (
