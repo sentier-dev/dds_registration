9c65913dcd6edc290cda7b3f39f844c2f1af7872
Chris Mutel <cmutel@gmail.com>
2025-02-26 10:19:53 +0100

    Add currency conversion for SGD

--- dds_registration/views/helpers/stripe_amounts.py
+++ dds_registration/views/helpers/stripe_amounts.py
@@ -5,17 +5,20 @@ def get_stripe_amount_for_currency(amount: int, currency: str, convert_basic_uni
     if convert_basic_unit:
         amount = get_stripe_basic_unit(amount=amount, currency=currency)
     if currency == "USD":
-        # Stripe fee is 2.9%, currency conversion loss is 1%
-        return round(30 + (1 + 0.029 + 0.01) * amount)
+        # Stripe fee is 2.9%, currency conversion loss is 1.5%
+        return round(30 + (1 + 0.029 + 0.015) * amount)
     elif currency == "CHF":
-        # Stripe fee is 2.9%, currency conversion loss is 1%
-        return round(30 + (1 + 0.029 + 0.01) * amount)
+        # Stripe fee is 2.9%, currency conversion loss is 1.5%
+        return round(30 + (1 + 0.029 + 0.015) * amount)
     elif currency == "EUR":
         # Stripe fee is 1.5%
         return round(25 + (1 + 0.015) * amount)
     elif currency == "CAD":
-        # Stripe fee is 2.9%, currency conversion loss is 1%
-        return round(30 + (1 + 0.029 + 0.01) * amount)
+        # Stripe fee is 2.9%, currency conversion loss is 1.5%
+        return round(30 + (1 + 0.029 + 0.015) * amount)
+    elif currency == "SGD":
+        # Stripe fee is 3.4%, currency conversion loss is 1.5%
+        return round(50 + (1 + 0.034 + 0.015) * amount)
     else:
         raise ValueError(f"Unrecognized currency {currency}")
 
@@ -24,14 +27,14 @@ def get_stripe_basic_unit(amount: float, currency: str) -> int:
     """Stripe wants numbers in lower divisible unit.
 
     This varies by country."""
-    if currency in ("CAD", "CHF", "EUR", "CAD"):
+    if currency in ("CAD", "CHF", "EUR", "CAD", "SGD"):
         return round(amount * 100)
     else:
         raise ValueError(f"Unrecognized currency {currency}")
 
 
 def convert_from_stripe_units(amount: float, currency: str) -> float:
-    if currency in ("CAD", "CHF", "EUR", "CAD"):
+    if currency in ("CAD", "CHF", "EUR", "CAD", "SGD"):
         return round(amount / 100, 2)
     else:
         raise ValueError(f"Unrecognized currency {currency}")
