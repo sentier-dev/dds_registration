cfe7f1d8be67acac82696f750081a3814718789a
Bruno Barcarol Guimarães <bbguimaraes@bbguimaraes.com>
2025-08-19 16:35:26 +0200

    Fix `field_validators` override
    ```
      File "djf_surveys/validators.py", line 45, in TermsTextAreaValidator
        max_length = field_validators['max_length']['text_area']
                     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~^^^^^^^^^^^^^
    KeyError: 'text_area'
    ```

--- dds_registration/apps.py
+++ dds_registration/apps.py
@@ -14,4 +14,6 @@ class RegConfig(AppConfig):
             stripe.api_key = settings.STRIPE_SECRET_KEY
 
         SURVEY_FIELD_VALIDATORS["min_length"]["text_area"] = 3
-        SURVEY_FIELD_VALIDATORS["max_length"] = {"email": 250, "text": 2500, "url": 500}
+        SURVEY_FIELD_VALIDATORS["max_length"].update(
+            email=250, text=2500, url=500,
+        )
